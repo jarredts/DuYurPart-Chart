f79dd29e3ade103074567bb517fb942b69a642bb
jarredts <jarredts@gmail.com>
2026-08-14 11:15:06 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,32 @@
-const CACHE_NAME = 'chores-cache-v5';
-const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+// Change this version whenever you update app files
+const CACHE_NAME = 'v1.0.1';
+
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js'
+];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) =>
-      Promise.all(CORE_ASSETS.map((url) =>
-        fetch(url, { cache: 'reload' }).then((response) => cache.put(url, response)).catch(() => {})
-      ))
-    )
-  );
+  // Force active service worker to become the active one immediately
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
 self.addEventListener('activate', (event) => {
+  // Delete older caches
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
-  if (url.origin === self.location.origin) {
-    event.respondWith(
-      caches.match(event.request).then((cached) => cached || fetch(event.request))
-    );
-  } else {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(event.request))
-    );
-  }
 });
