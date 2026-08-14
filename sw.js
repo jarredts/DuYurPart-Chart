6ef1c7e065e32ba8f9444857b40af27f59f08901
jarredts <jarredts@gmail.com>
2026-08-14 15:48:21 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('https://www.gstatic.com/firebasejs/10.13.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.13.0/firebase-messaging-compat.js');
 
-const CACHE_NAME = 'chores-cache-v10';
+const CACHE_NAME = 'chores-cache-v11';
 const CORE_ASSETS = [
   './',
   './index.html',
